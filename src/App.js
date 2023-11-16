6b807b2f06dd6ea8ef81341249afe64dee412ba8
ROHAN4535 <grohan468@gmail.com>
2023-11-16 12:35:49 +0530

    Signup and Authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import AuthForm from "./Components/Auth/AuthForm";
+
+import Layout from "./Components/Layout/Layout";
+import UserProfile from "./Components/Profile/UserProfile";
+import AuthPage from "./Components/Pages/AuthPage";
+import HomePage from "./Components/Pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/profile" element={<UserProfile />} />
+      </Routes>
+      <AuthForm/>
+    </Layout>
   );
 }
 
