e02eb860b29e39fdec140479502464b9d133b4f0
ROHAN4535 <grohan468@gmail.com>
2023-11-17 19:51:13 +0530

    Addressing security and logout issues

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import AuthForm from "./Components/Auth/AuthForm";
 
 import Layout from "./Components/Layout/Layout";
 import UserProfile from "./Components/Profile/UserProfile";
 import AuthPage from "./Components/Pages/AuthPage";
 import HomePage from "./Components/Pages/HomePage";
+import { useContext } from "react";
+import AuthContext from "./Components/Store/Auth-Context";
 
 function App() {
+  const authCtx = useContext(AuthContext);
   return (
     <Layout>
       <Routes>
         <Route path="/home" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
-        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/profile" element={authCtx.isLoggedIn ? <UserProfile/> : <Navigate to="/auth"/>}/>
       </Routes>
       <AuthForm/>
     </Layout>
